a9eab468e0fa9d679b4579f74456bdbc403ea785
User <drjagin-vadim@rambler.ru>
2020-04-17 07:44:46 +0600

    DeleteNews

---
+++ public/controllers/news.php
@@ -0,0 +1,18 @@
+<?php
+
+    class news
+    {
+        Public static function getList(){
+            $all = DB::run("SELECT id, name FROM tbnews")->fetchall();
+            return $all;
+        }
+
+        Public static function getByID($id){
+            $row = DB::run("SELECT * FROM tbnews WHERE id=?", array($id))->fetch();
+            return $row ;
+        }
+
+        public static function delNews($id){
+            $del = DB::run("DELETE FROM tbnews WHERE id=?", array($id));
+        }
+    }
