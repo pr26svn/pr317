e6f7a615ebd9f84b0d1b7167f819852951c52c79
LinaSaveleva <63015889+LinaSaveleva@users.noreply.github.com>
2020-04-24 14:20:54 +0600

    Add files via upload

---
+++ public/news.php
@@ -0,0 +1,24 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/models/DB.php");
+require($_SERVER["DOCUMENT_ROOT"]."/controllers/news.php");
+$newsList=news::getList();
+?>
+
+<table>
+    <tr><td>ID</td><td>Название новости </td><td>Описание</td><td>Действия</td></tr>
+    <?php foreach($newsList as $arNews):?>
+        <tr>
+            <td><?=$arNews["id"];?></td>
+            <td><?=$arNews["name"];?></td>
+            <td><?=$arNews["description"];?></td>
+            <td><a href="/news.php?id=<?=$arNews["id"];?>">
+                    Удалить
+                </a><br/>
+                <a href="/editNews.php?id=<?=$arNews["id"];?>">
+                    Изменить</a>
+            </td>
+        </tr>
+    <?php endforeach;?>
+</table>
+
+
---
+++ test.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 24 2020 г., 11:16
+-- Версия сервера: 5.5.62
+-- Версия PHP: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(30) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `news`
+--
+
+INSERT INTO `news` (`id`, `name`, `description`) VALUES
+(1, 'машина', 'сломалась'),
+(2, 'газета', 'сгорела');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(30) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
